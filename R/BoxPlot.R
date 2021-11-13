2ea263e14d335a2980adbdd1ede992b60e4e1c93
katie <katelyn.parrish@selu.edu>
2021-11-13 02:00:08 +0000

    changed box plot to line plot; added input, output, and return values for the line plot

--- R/BoxPlot.R
+++
@@ -1,5 +0,0 @@
-ggplot(data = ants, mapping = aes(x = subfamily, y = min_ma)) +
-  geom_boxplot(alpha = 0) +
-  geom_jitter(alpha = 0.3, color = "pink")
-## I just made this to see if my idea was even plot-able! Temporary
-
