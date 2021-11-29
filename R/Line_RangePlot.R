0c7f0afd0577f372fd0537f32f48de1929584901
katie <katelyn.parrish@selu.edu>
2021-11-29 01:11:10 +0000

    adding documentation for line range plot function

--- R/Line_RangePlot.R
+++ R/Line_RangePlot.R
@@ -1,10 +1,32 @@
-ants <- read_csv("/cloud/project/data/FossilAnts.csv")
+#' Function for plotting a line range function using the 'ggplot2' package.
+#' 
+#' This function includes an x-axis label rotation at -45 degrees for easy axis
+#' reading.
+#' 
+#' @param data a data frame
+#' @param x_axis the column in the data frame to be plotted on the x-axis of the graph
+#' @param y-min the column in the data frame to be plotted with the y-max data
+#' @param y-max the column in the data frame to be plotted with the y-min data
+#' 
+#' @return plot The line range plot of the selected data
+#' 
+#' @usage
+#' 
+#' Line range plot using the 'change_na_rm' function to remove NA values with additional y-axis labeling 
+#' ants %>% 
+#'  change_na_rm("None") %>% 
+#'  line_range_plot(Tribe, min_ma, max_ma) +
+#'  labs(y = "Age (in Ma)")
 
-line_range_plot <- function(dataframe, x_axis, y_min, y_max) {
-  output_plot <- ggplot(data = dataframe, aes(x = {{x_axis}}, ymin = {{y_min}}, ymax = {{y_max}})) +
-    geom_linerange()
+line_range_plot <- function(data, x_axis, y_min, y_max) {
+  output_plot <- ggplot(data = data, aes(x = {{x_axis}}, ymin = {{y_min}}, ymax = {{y_max}})) +
+    geom_linerange(size = 1) +
+    theme(axis.text.x = element_text(angle = -45, vjust = 2, hjust = 0))
   print(output_plot)
 }
+
+
+
 
 
 
